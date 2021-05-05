7a60467c5da4ad9813b3b156ec9858cdb590b05c
Mihai <kubikweg@gmail.com>
2021-05-05 21:01:37 +0300

    Configure with GitLab

--- src/DomainTests/DogTests.java
+++ src/DomainTests/DogTests.java
@@ -10,5 +10,6 @@ public class DogTests {
     public void getRace_DogExists_returnsTheRace(){
         Dog ia = new Dog("Barks","Malamut",true, 20,"Alaska");
         assert(ia.getRace().equals("Malamut"));
+
     }
 }
